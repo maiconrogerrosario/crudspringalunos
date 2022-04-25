9db1ac4659798e1a84308cbbc61100901f73e3cc
maiconrogerrosario <maiconrogerrosario@gmail.com>
2022-04-25 16:39:52 -0300

    Criação de Projetos

---
+++ crudspringalunos/src/main/java/com/alunos/CrudspringalunosApplication.java
@@ -0,0 +1,13 @@
+package com.alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringalunosApplication.class, args);
+	}
+
+}
